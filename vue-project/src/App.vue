64ea767b7009e83e008631d3eac52b27c81d268f
m1ns2o <srjms0128@gmail.com>
2022-08-31 22:27:23 +0900

    no one page

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,50 +1,50 @@
 <template>
 	<v-app>
 		<div id="app">
-			<full-page :options="options" id="fullpage" ref="fullpage">
-				<div class="section" id="page1">
-					<div class="text">
-						<h1>인편 보내기</h1>
-						<div class="info">
-							<h3>※ 더캠프의 공식서비스가 아닙니다 ※</h3>
-							<!-- <h3>Section 1</h3> -->
-						</div>
-						<div class="soldier">
-							<h5>생년월일 : 2002 01 28</h5>
-							<h5>입대일 : 2022 09 20</h5>
-							<h5>입영부대 : 55사단 신병교육대</h5>
-						</div>
+			<!-- <full-page :options="options" id="fullpage" ref="fullpage"> -->
+			<div class="section" id="page1">
+				<div class="text">
+					<h1>인편 보내기</h1>
+					<div class="info">
+						<h3>※ 더캠프의 공식서비스가 아닙니다 ※</h3>
+						<!-- <h3>Section 1</h3> -->
 					</div>
-					<div class="img">
-						<img :src="img" id="img" />
-						<img :src="arrow" id="arrow" />
+					<div class="soldier">
+						<h5>생년월일 : 2002 01 28</h5>
+						<h5>입대일 : 2022 09 20</h5>
+						<h5>입영부대 : 55사단 신병교육대</h5>
 					</div>
 				</div>
-				<div class="section" id="page2">
-					<v-form v-model="valid" ref="form">
-						<v-container>
-							<v-row>
-								<v-col cols="12" md="6">
-									<v-text-field
-										v-model="name"
-										:rules="nameRules"
-										:counter="10"
-										label="이름"
-										required
-										outlined
-									></v-text-field>
-								</v-col>
-								<v-col cols="12" md="6">
-									<v-text-field
-										v-model="title"
-										:rules="titleRules"
-										:counter="50"
-										label="제목"
-										required
-									></v-text-field>
-								</v-col>
+				<div class="img">
+					<img :src="img" id="img" />
+					<!-- <img :src="arrow" id="arrow" /> -->
+				</div>
+			</div>
+			<div class="section" id="page2">
+				<v-form v-model="valid" ref="form">
+					<v-container>
+						<v-row>
+							<v-col cols="12" md="6">
+								<v-text-field
+									v-model="name"
+									:rules="nameRules"
+									:counter="10"
+									label="이름"
+									required
+									outlined
+								></v-text-field>
+							</v-col>
+							<v-col cols="12" md="6">
+								<v-text-field
+									v-model="title"
+									:rules="titleRules"
+									:counter="50"
+									label="제목"
+									required
+								></v-text-field>
+							</v-col>
 
-								<!-- <v-col cols="12" md="4">
+							<!-- <v-col cols="12" md="4">
 						<v-text-field
 							v-model="text"
 							:rules="emailRules"
@@ -54,32 +54,32 @@
 						></v-text-field>
 					</v-col> -->
 
-								<v-col>
-									<v-textarea
-										v-model="text"
-										:rules="textRules"
-										:counter="1500"
-										auto-grow="true"
-										name="input-7-1"
-										label="내용"
-										rows="10"
-									></v-textarea>
-									<v-btn
-										:disabled="!valid"
-										color="success"
-										class="mr-4"
-										@click="validate"
-									>
-										편지 보내기
-									</v-btn>
-								</v-col>
-							</v-row>
-						</v-container>
+							<v-col>
+								<v-textarea
+									v-model="text"
+									:rules="textRules"
+									:counter="1500"
+									auto-grow="true"
+									name="input-7-1"
+									label="내용"
+									rows="10"
+								></v-textarea>
+								<v-btn
+									:disabled="!valid"
+									color="success"
+									class="mr-4"
+									@click="validate"
+								>
+									편지 보내기
+								</v-btn>
+							</v-col>
+						</v-row>
+					</v-container>
 
-						<!-- <v-btn class="mr-4" @click="submit"> submit </v-btn> -->
-					</v-form>
-				</div>
-			</full-page>
+					<!-- <v-btn class="mr-4" @click="submit"> submit </v-btn> -->
+				</v-form>
+			</div>
+			<!-- </full-page> -->
 		</div>
 	</v-app>
 </template>
@@ -127,15 +127,15 @@ export default {
 			(v) => !!v || "내용을 작성해주세요.",
 			(v) => v.length <= 1500 || "내용은 1490자 이하여야합니다. ",
 		],
-		options: {
-			licenseKey: "YOUR_KEY_HERE",
-			// afterLoad: this.afterLoad,
-			scrollOverflow: true,
-			// scrollBar: true,
-			menu: "#menu",
-			navigation: false,
-			// anchors: ["page1", "page3"],
-		},
+		// options: {
+		// 	licenseKey: "YOUR_KEY_HERE",
+		// 	// afterLoad: this.afterLoad,
+		// 	scrollOverflow: true,
+		// 	// scrollBar: true,
+		// 	menu: "#menu",
+		// 	navigation: false,
+		// 	// anchors: ["page1", "page3"],
+		// },
 	}),
 	methods: {
 		validate() {
@@ -159,71 +159,71 @@ export default {
 				});
 		},
 
-		afterLoad() {
-			console.log("After load");
-		},
+		// 	afterLoad() {
+		// 		console.log("After load");
+		// 	},
 
-		addSection(e) {
-			e.preventDefault();
-			var newSectionNumber =
-				document.querySelectorAll(".fp-section").length + 1;
+		// 	addSection(e) {
+		// 		e.preventDefault();
+		// 		var newSectionNumber =
+		// 			document.querySelectorAll(".fp-section").length + 1;
 
-			// creating the section div
-			var section = document.createElement("div");
-			section.className = "section";
-			section.innerHTML = `<h3>Section ${newSectionNumber}</h3>`;
+		// 		// creating the section div
+		// 		var section = document.createElement("div");
+		// 		section.className = "section";
+		// 		section.innerHTML = `<h3>Section ${newSectionNumber}</h3>`;
 
-			// adding section
-			document.querySelector("#fullpage").appendChild(section);
+		// 		// adding section
+		// 		document.querySelector("#fullpage").appendChild(section);
 
-			// creating the section menu element
-			var sectionMenuItem = document.createElement("li");
-			sectionMenuItem.setAttribute(
-				"data-menuanchor",
-				"page" + newSectionNumber
-			);
-			sectionMenuItem.innerHTML = `<a href="#page${newSectionNumber}">Section${newSectionNumber}</a>`;
+		// 		// creating the section menu element
+		// 		var sectionMenuItem = document.createElement("li");
+		// 		sectionMenuItem.setAttribute(
+		// 			"data-menuanchor",
+		// 			"page" + newSectionNumber
+		// 		);
+		// 		sectionMenuItem.innerHTML = `<a href="#page${newSectionNumber}">Section${newSectionNumber}</a>`;
 
-			// adding it to the sections menu
-			var sectionsMenuItems = document.querySelector("#menu");
-			sectionsMenuItems.appendChild(sectionMenuItem);
+		// 		// adding it to the sections menu
+		// 		var sectionsMenuItems = document.querySelector("#menu");
+		// 		sectionsMenuItems.appendChild(sectionMenuItem);
 
-			// adding anchor for the section
-			this.options.anchors.push(`page${newSectionNumber}`);
+		// 		// adding anchor for the section
+		// 		this.options.anchors.push(`page${newSectionNumber}`);
 
-			// we have to call `update` manually as DOM changes won't fire updates
-			// requires the use of the attribute ref="fullpage" on the
-			// component element, in this case, <full-page>
-			// ideally, use an ID element for that element too
-			this.$refs.fullpage.build();
-		},
+		// 		// we have to call `update` manually as DOM changes won't fire updates
+		// 		// requires the use of the attribute ref="fullpage" on the
+		// 		// component element, in this case, <full-page>
+		// 		// ideally, use an ID element for that element too
+		// 		this.$refs.fullpage.build();
+		// 	},
 
-		removeSection() {
-			var sections = document
-				.querySelector("#fullpage")
-				.querySelectorAll(".fp-section");
-			var lastSection = sections[sections.length - 1];
+		// 	removeSection() {
+		// 		var sections = document
+		// 			.querySelector("#fullpage")
+		// 			.querySelectorAll(".fp-section");
+		// 		var lastSection = sections[sections.length - 1];
 
-			// removing the last section
-			lastSection.parentNode.removeChild(lastSection);
+		// 		// removing the last section
+		// 		lastSection.parentNode.removeChild(lastSection);
 
-			// removing the last anchor
-			this.options.anchors.pop();
+		// 		// removing the last anchor
+		// 		this.options.anchors.pop();
 
-			// removing the last item on the sections menu
-			var sectionsMenuItems = document.querySelectorAll("#menu li");
-			var lastItem = sectionsMenuItems[sectionsMenuItems.length - 1];
-			lastItem.parentNode.removeChild(lastItem);
-		},
+		// 		// removing the last item on the sections menu
+		// 		var sectionsMenuItems = document.querySelectorAll("#menu li");
+		// 		var lastItem = sectionsMenuItems[sectionsMenuItems.length - 1];
+		// 		lastItem.parentNode.removeChild(lastItem);
+		// 	},
 
-		toggleNavigation() {
-			this.options.navigation = !this.options.navigation;
-		},
+		// 	toggleNavigation() {
+		// 		this.options.navigation = !this.options.navigation;
+		// 	},
 
-		toggleScrollbar() {
-			console.log("Changing scrollbar...");
-			this.options.scrollBar = !this.options.scrollBar;
-		},
+		// 	toggleScrollbar() {
+		// 		console.log("Changing scrollbar...");
+		// 		this.options.scrollBar = !this.options.scrollBar;
+		// 	},
 	},
 };
 </script>
@@ -260,7 +260,7 @@ button {
 	max-width: 900px;
 	margin-right: auto;
 	margin-left: auto;
-	display: flex;
+	/* display: flex; */
 	padding-top: 0;
 	/* justify-content: flex-start; */
 	/* margin-top: 0;
@@ -280,11 +280,11 @@ form {
 }
 
 #page2 {
-	display: flex;
-	/* flex-direction: column;
-	/* align-items: center; */
-	/* margin-top: 20px; */
-	justify-content: flex-start;
+	/* display: flex;
+	align-items: center;
+	margin-top: 20px;
+	justify-content: flex-start; */
+	margin-bottom: 10%;
 }
 
 .text {
@@ -317,7 +317,6 @@ form {
 
 /* @media screen and (min-width: 768px), (orientation: landscape) {
 	.section {
-		border-radius: 1%;
 		box-shadow: 0px 2px 10px #d0d3d9;
 	}
 } */
